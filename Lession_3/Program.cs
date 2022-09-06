13c4fe74620937df2ff8ddd7a54d1714acdacdd0
Best Cat <kotova-irina11@rambler.ru>
2022-09-06 20:14:24 +0300

    задача_1

--- Lession_3/Program.cs
+++ Lession_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход координаты точки 
+//(X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+//в которой находится эта точка.
+Console.Write("Введите x: ");
+int x= int.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+int y= int.Parse(Console.ReadLine());
+
+if (x>0 && y>0)
+{
+    Console.WriteLine("1 четверть");
+}
+if (x<0 && y>0)
+{
+    Console.WriteLine("2 четверть");
+}
+if (x<0 && y<0)
+{
+    Console.WriteLine("3 четверть");
+}
+if (x>0 && y<0)
+{
+    Console.WriteLine("4 четверть");
+}
