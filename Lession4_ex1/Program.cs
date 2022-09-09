95e94d06753fe7ec30bdd73229ee9234de74e531
Best Cat <kotova-irina11@rambler.ru>
2022-09-09 20:17:24 +0300

    урок 4 задача сумма чисел от 1 до N

---
+++ Lession4_ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 24 на вход число А и выдает сумму чисел от 1 до А
+Console.WriteLine("Введите чтсло: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел = {GetSumNums(num)}");
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum = sum + number;
+        Console.WriteLine(sum); // выведение суммы в каждом цикле. можно num
+        number = number - 1;
+    }
+    return sum;
+}
