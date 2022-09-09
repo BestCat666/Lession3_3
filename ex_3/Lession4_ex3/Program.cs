87d2768b3b7ecc189ee09965715faca2441916a2
Best Cat <kotova-irina11@rambler.ru>
2022-09-09 22:25:15 +0300

    урок 4

---
+++ ex_3/Lession4_ex3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()); 
+int FacterialNum(int kol)
+{
+    int i = 1;
+    int facte = 1;
+    while ( i <= kol)
+    {
+        facte = i * facte;
+        //Console.WriteLine(number);
+        i++;
+    }
+    return facte;
+}
+Console.WriteLine($"{num} -> {FacterialNum(num)}");
+
+
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()); 
+
+int Factorial(int num)
+{
+    int count = num;
+    while (count > 1)
+    {
+        count--;
+        num *= count;
+        Console.WriteLine(num);
+    }
+    return num;
+}
+
+Console.WriteLine($"{number} -> {Factorial(number)}");
